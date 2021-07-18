e2907b4bb134a5bb9024a563b43e2943ed5f21f0
amr elsayed <amrmostafa1995@gmail.com>
2021-07-18 22:17:17 +0200

    BranchSums

--- src/AlgoExpert/BranchSums.java
+++ src/AlgoExpert/BranchSums.java
@@ -1,6 +1,7 @@
 package AlgoExpert;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BranchSums {
     public static class BinaryTree {
@@ -27,7 +28,7 @@ public class BranchSums {
     }
 
     static ArrayList<Integer> branchSums(BinaryTree root) {
-        if (root == null) return new ArrayList<Integer>();
+        if (root == null) return new ArrayList<>();
 
         ArrayList<Integer> list = new ArrayList<>();
         branchSums(root, list, 0);
@@ -35,6 +36,15 @@ public class BranchSums {
     }
 
     public static void main(String[] args) {
+
+//        tree =   1
+//                /     \
+//                2       3
+//                /   \   /   \
+//                4     5 6     7
+//                / \
+//                8   9
+
 
         BinaryTree tree = new BinaryTree(1);
         BinaryTree child1 = new BinaryTree(2);
@@ -59,5 +69,37 @@ public class BranchSums {
         child2.right = child6;
 
         System.out.println(branchSums(tree).toString());
+        System.out.println(branchSums2(tree).toString());
+    }
+
+    public static ArrayList<Integer> branchSums2(BinaryTree root) {
+        Stack<Pair> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Pair current;
+        stack.push(new Pair(root, root.value));
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+            if (current.node.left == null && current.node.right == null) {
+                list.add(current.runningSum);
+            }
+            if (current.node.right != null) {
+                stack.push(new Pair(current.node.right, current.runningSum + current.node.right.value));
+            }
+            if (current.node.left != null) {
+                stack.push(new Pair(current.node.left, current.runningSum + current.node.left.value));
+            }
+        }
+        return list;
+    }
+
+}
+
+class Pair {
+    BranchSums.BinaryTree node;
+    int runningSum;
+
+    Pair(BranchSums.BinaryTree node, int sum) {
+        this.node = node;
+        this.runningSum = sum;
     }
 }
