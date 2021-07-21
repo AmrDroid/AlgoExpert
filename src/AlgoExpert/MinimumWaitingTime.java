bf551fa4456453ada65edec6ba2b182447ca2f80
amr elsayed <amrmostafa1995@gmail.com>
2021-07-21 17:02:31 +0200

    MinimumWaitingTime

--- src/AlgoExpert/MinimumWaitingTime.java
+++ src/AlgoExpert/MinimumWaitingTime.java
@@ -6,23 +6,39 @@ public class MinimumWaitingTime {
 
 
     //O(nlog(n)) time | O(1) space, where n is the number of queries.
-    public static int minimumWaitingTime(int []arr)
-    {
+    public static int minimumWaitingTime(int[] arr) {
         Arrays.sort(arr);
-        int totalWaitingTime=0;
-        int waitingTime=0;
+        int totalWaitingTime = 0;
+        int waitingTime = 0;
 
-        for(int duration : arr)
-        {
-            totalWaitingTime+=waitingTime;
-            waitingTime+=duration;
+        for (int duration : arr) {
+            totalWaitingTime += waitingTime;
+            waitingTime += duration;
         }
         return totalWaitingTime;
 
     }
+
     public static void main(String[] args) {
 
-        int []arr={1, 3, 2};
+        int[] arr = {1, 3, 2};
         System.out.println(minimumWaitingTime(arr));
+        System.out.println(minimumWaitingTime2(arr));
     }
+
+//O(nlog(n)) time | O(1) space, where n is the number of queries.
+   public static int minimumWaitingTime2(int[] queries) {
+
+        // 0 + 1 + (1+2)
+        Arrays.sort(queries);
+        int totalWaitingTime = 0;
+        for (int idx = 0; idx < queries.length; idx++) {
+            int duration = queries[idx];
+            int queriesLeft = queries.length - (idx + 1);
+            totalWaitingTime += duration * queriesLeft;
+        }
+
+        return totalWaitingTime;
     }
+
+}
