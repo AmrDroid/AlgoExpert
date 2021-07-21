e4f32a3bc5ef84cb3062d3152058ea88acce5667
amr elsayed <amrmostafa1995@gmail.com>
2021-07-21 18:50:10 +0200

    NthFibonacci

--- src/AlgoExpert/NthFibonacci.java
+++ src/AlgoExpert/NthFibonacci.java
@@ -23,17 +23,51 @@ public class NthFibonacci {
 
         if (map.containsKey(i)) return map.get(i);
 
-        map.put(i,fibonacci2(i-1,map)+fibonacci2(i-2,map));
+        map.put(i, fibonacci2(i - 1, map) + fibonacci2(i - 2, map));
 
         return map.get(i);
     }
+
     public static int fibonacci_2(int i) {
-        return fibonacci2(i,new HashMap<>());
+        return fibonacci2(i, new HashMap<>());
     }
-        public static void main(String[] args) {
+
+
+    public static int fibonacci3(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        int[] memo = new int[n];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i < n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n - 1] + memo[n - 2];
+    }
+
+
+    public static int fibonacci4(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+
+        }
+        return a + b;
+    }
+
+    public static void main(String[] args) {
 
         System.out.println(fibonacci(10));
         System.out.println(fibonacci_2(10));
+        System.out.println(fibonacci3(10));
+        System.out.println(fibonacci4(10));
     }
 
 
