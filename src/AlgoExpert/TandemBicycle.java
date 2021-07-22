6ec86235c6cf12e26705d82b17a533c4a673915d
amr elsayed <amrmostafa1995@gmail.com>
2021-07-22 17:36:24 +0200

    TandemBicycle

--- src/AlgoExpert/TandemBicycle.java
+++ src/AlgoExpert/TandemBicycle.java
@@ -30,9 +30,35 @@ public class TandemBicycle {
 
         System.out.println(tandemBicycle(red, blue, false));
         System.out.println(tandemBicycle(red, blue, true));
+        System.out.println(tandemBicycle2(new int[]{8, 5, 3}, new int[]{7, 2, 4}, false));
+        System.out.println(tandemBicycle2(new int[]{8, 5, 3}, new int[]{7, 2, 4}, true));
     }
 
+    public static int tandemBicycle2(int[] redShirtsSpeed, int[] blueShirtsSpeed, boolean fastest) {
+        Arrays.sort(redShirtsSpeed);
+        Arrays.sort(blueShirtsSpeed);
 
+        if (fastest)
+            reverse(blueShirtsSpeed);
+
+        int totalSpeed = 0;
+        for (int i = 0; i < redShirtsSpeed.length; i++)
+            totalSpeed += Math.max(redShirtsSpeed[i], blueShirtsSpeed[i]);
+
+        return totalSpeed;
+    }
+
+    private static void reverse(int[] blueShirtsSpeed) {
+        int start = 0;
+        int end = blueShirtsSpeed.length - 1;
+        while (start < end) {
+            int temp = blueShirtsSpeed[start];
+            blueShirtsSpeed[start] = blueShirtsSpeed[end];
+            blueShirtsSpeed[end] = temp;
+            start++;
+            end--;
+        }
+    }
 
 
 
