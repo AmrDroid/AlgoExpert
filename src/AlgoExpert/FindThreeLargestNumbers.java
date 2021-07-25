ed71f6269b4b3df5f14b7be45c984b0e7ef1190c
amr elsayed <amrmostafa1995@gmail.com>
2021-07-25 12:55:58 +0200

    FindThreeLargestNumbers

--- src/AlgoExpert/FindThreeLargestNumbers.java
+++ src/AlgoExpert/FindThreeLargestNumbers.java
@@ -1,9 +1,9 @@
 package AlgoExpert;
 
 public class FindThreeLargestNumbers {
+    //O(n) time | O(1) space, where n is the length of the input array.
     public static int[] findThreeLargestNumbers(int[] array) {
-
-        int []arr = new int[3];
+        int[] arr = new int[3];
         int index = 0;
         for (int i = array.length - 1; i >= array.length - 3; i--) {
             int largestNumIdx = 0;
@@ -20,18 +20,55 @@ public class FindThreeLargestNumbers {
         return arr;
     }
 
-    static void printarray(int[] arr) {
+    ////////////////////////////////////////////////////////////////////
+//O(n) time | O(1) space, where n is the length of the input array.
+    static int[] findThreeLargestNumbers2(int[] array) {
+        int[] threeLargestNums = new int[3];
+
+        for (int num : array) {
+            updateThreeLargestNums(threeLargestNums, num);
+        }
+        return threeLargestNums;
+    }
+
+    static void updateThreeLargestNums(int[] threeLargestNums, int num) {
+        int thirdLargestNum = threeLargestNums[0];
+        int secondLargestNum = threeLargestNums[1];
+        int largestNum = threeLargestNums[2];
+        if (num > largestNum) {
+            shiftAndUpdate(threeLargestNums, num, 2);
+        } else if (num > secondLargestNum) {
+            shiftAndUpdate(threeLargestNums, num, 1);
+        } else if (num > thirdLargestNum) {
+            shiftAndUpdate(threeLargestNums, num, 0);
+        }
+    }
+
+    //5 7 8
+    static void shiftAndUpdate(int[] array, int num, int idx) {
+        for (int i = 0; i <= idx; i++) {
+            if (i == idx) {
+                array[i] = num;
+            } else {
+                array[i] = array[i + 1];
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{8, 4, 5, 1, 2, 7, 3};
+        int[] arr = findThreeLargestNumbers(array);
+
+        printArr(arr);
+        printArr(findThreeLargestNumbers2(array));
+
+    }
+
+
+    static void printArr(int []arr) {
         for (int i : arr)
             System.out.print(i + "  ");
         System.out.println();
     }
-
-    public static void main(String[] args) {
-        int[] array = new int[]{8, 4, 5, 1, 2, 7, 3};
-        int []arr = findThreeLargestNumbers(array);
-        for (int i : arr)
-            System.out.print(i + "  ");
-
-    }
-
 }
