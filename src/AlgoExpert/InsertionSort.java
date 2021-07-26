70590e1be1503a25fc277789a630a2be12f16298
amr elsayed <amrmostafa1995@gmail.com>
2021-07-26 13:54:57 +0200

    InsertionSort

--- src/AlgoExpert/InsertionSort.java
+++ src/AlgoExpert/InsertionSort.java
@@ -2,21 +2,50 @@ package AlgoExpert;
 
 public class InsertionSort {
 
-//    Time Complexity: O(n^2)
-//    Space: O(1)
+    //    Time Complexity: O(n^2)
+    //    Space: O(1)
     public static int[] insertionSort(int[] array) {
-        if (array == null || array.length == 0) return new int[] {};
+        if (array == null || array.length == 0) return new int[]{};
         for (int i = 1; i < array.length; i++) {
             int j = i;
             while (j > 0 && array[j] < array[j - 1]) {
                 int temp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = temp;
-                --j;
+                j--;
             }
         }
 
         return array;
+    }
+
+    public static int[] insertionSort2(int[] arr) {
+        if (arr == null || arr.length == 0) return new int[]{};
+        for (int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+            printArr(arr);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{8, 4, 5, 1, 2, 7, 3};
+     //   printArr(insertionSort(array));
+        printArr(insertionSort2(array));
+
+    }
+
+
+    static void printArr(int[] arr) {
+        for (int i : arr)
+            System.out.print(i + "  ");
+        System.out.println();
     }
 
 
