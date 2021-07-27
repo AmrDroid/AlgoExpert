85b3729b80d034314e3583eac3027d572fbc9371
amr elsayed <amrmostafa1995@gmail.com>
2021-07-27 20:08:13 +0200

    FirstNonRepeatingCharacter

--- src/AlgoExpert/FirstNonRepeatingCharacter.java
+++ src/AlgoExpert/FirstNonRepeatingCharacter.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FirstNonRepeatingCharacter {
-    public static int firstNonRepeatingCharacter(String string) {
+    public static int firstNonRepeating(String string) {
         // Write your code here.
         Map<Character, Integer> map = new HashMap<>();
         for (char c : string.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
@@ -16,10 +16,37 @@ public class FirstNonRepeatingCharacter {
         return -1;
     }
 
+
+    public static int firstNonRepeating2(String str) {
+        int[] fi = new int [256];
+
+        for(int i = 0; i<256; i++)
+            fi[i] = -1;
+
+        for(int i = 0; i<str.length(); i++) {
+            if(fi[str.charAt(i)] == -1) {
+                fi[str.charAt(i)] = i;
+            }else {
+                fi[str.charAt(i)] = -2;
+            }
+        }
+
+        int res =  Integer.MAX_VALUE;
+
+        for(int i = 0; i<256; i++) {
+            if(fi[i] >= 0)
+                res = Math.min(res, fi[i]);
+        }
+        if(res ==  Integer.MAX_VALUE) return -1;
+        else return res;
+    }
+
+
     public static void main(String[] args) {
         String text = "GeeksforGeeks";
 
-        System.out.println(text.charAt(firstNonRepeatingCharacter(text)));
+        System.out.println(text.charAt(firstNonRepeating(text)));
+        System.out.println(text.charAt(firstNonRepeating2(text)));
     }
 
 }
