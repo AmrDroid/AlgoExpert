f18800258d6d945cc3efbdb8199cc239a4ed94c1
amr elsayed <amrmostafa1995@gmail.com>
2021-07-30 15:35:53 +0200

    SpiralTraverse

--- src/AlgoExpert/Medium/SpiralTraverse.java
+++ src/AlgoExpert/Medium/SpiralTraverse.java
@@ -28,16 +28,15 @@ public class SpiralTraverse {
         }
         return list;
     }
+
     public static void main(String[] args) {
 
-        int[][] input = {
-                {1, 2, 3, 4, 5},
-                {6, 7, 8, 9, 10},
-                {1, 2, 3, 4, 5},
-                {6, 7, 8, 9, 10},
-                {1, 2, 3, 4, 5},
-                {0, 0, 0, 0, 0,}
-        };
+        int input[][] =
+                        {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+
         List<Integer> list = spiralTraverse(input);
 
         for (int item : list)
