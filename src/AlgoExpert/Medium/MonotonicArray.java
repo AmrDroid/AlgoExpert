df7d85b2885fe2e71c9ea5935858d1c2500caf0a
amr elsayed <amrmostafa1995@gmail.com>
2021-07-30 14:21:07 +0200

    MonotonicArray

---
+++ src/AlgoExpert/Medium/MonotonicArray.java
@@ -0,0 +1,42 @@
+package AlgoExpert.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonotonicArray {
+
+
+    private static boolean isMonotonic(List<Integer> numbers) {
+        if (numbers == null || numbers.size()<= 2) return true;
+
+        boolean increasing = false;
+        if (numbers.get(0) < numbers.get(1))
+            increasing = true;
+        if (numbers.get(0) == numbers.get(1) && numbers.get(1) < numbers.get(2))
+            increasing = true;
+
+        for (int i = 2; i < numbers.size(); i++) {
+            if (increasing && numbers.get(i) < numbers.get(i - 1))
+                return false;
+            else if (!increasing && numbers.get(i) > numbers.get(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> A = new ArrayList<>();
+        A.add(2);
+        A.add(2);
+        A.add(4);
+        A.add(4);
+        A.add(5);
+        A.add(6);
+        A.add(7);
+        A.add(8);
+
+        System.out.print(isMonotonic(A));
+
+    }
+
+}
