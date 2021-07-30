fdf0bcc779abfea8a5e4c514f2809c1a8ef32366
amr elsayed <amrmostafa1995@gmail.com>
2021-07-30 14:05:47 +0200

    MoveElementToEnd

--- src/AlgoExpert/Medium/MoveElementToEnd.java
+++ src/AlgoExpert/Medium/MoveElementToEnd.java
@@ -1,11 +1,13 @@
 package AlgoExpert.Medium;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class MoveElementToEnd {
-    private static Object[] moveElementToEnd(int[] numbers, int i) {
+    private static List<Integer> moveElementToEnd(List<Integer> numbers, int i) {
 
-        if (numbers.length == 0) return null;
+        if (numbers.size() == 0) return null;
 
         ArrayList<Integer> list = new ArrayList<>();
         int count = 0;
@@ -19,16 +21,50 @@ public class MoveElementToEnd {
             list.add(i);
             count--;
         }
-        return list.toArray();
+        return list;
 
+    }
+    ///////////////
+
+    public static List<Integer> moveElementToEnd2(List<Integer> array, int toMove) {
+        int lastIdx = array.size() - 1, firstIdx = 0;
+
+        while (firstIdx < lastIdx) {
+            while (lastIdx > firstIdx && array.get(lastIdx) == toMove)
+                --lastIdx;
+
+            if (array.get(firstIdx) == toMove) {
+                array.set(firstIdx, array.get(lastIdx));
+                array.set(lastIdx, toMove);
+                --lastIdx;
+            }
+
+            ++firstIdx;
+        }
+
+        return array;
     }
 
 
     public static void main(String[] args) {
-        int A[] = {1, 2, 5, 4, 5, 6, 5, 8, 5, 4, 45, 6, 10, 8};
+        List<Integer> A = new ArrayList<>();
+        A.add(1);
+        A.add(2);
+        A.add(5);
+        A.add(4);
+        A.add(5);
+        A.add(6);
+        A.add(5);
+        A.add(8);
 
-        Object[] res = moveElementToEnd(A, 5);
-        for (Object item : res)
+        List<Integer> res = moveElementToEnd(A, 5);
+        for (int item : res)
+            System.out.print(item + "    ");
+
+        System.out.println();
+
+        List<Integer> res2 = moveElementToEnd2(A, 5);
+        for (int item : res2)
             System.out.print(item + "    ");
 
     }
